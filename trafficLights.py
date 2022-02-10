cfdccc2295ceb827e18492ec0a498352edd9a009
Martina Yusuf <martinayusuf@Martinas-MacBook-Pro-2.local>
2022-02-10 09:55:50 +0000

    week2 classroom demo

---
+++ trafficLights.py
@@ -0,0 +1,85 @@
+#light = input("What colour do you see on the traffic light? ")
+#
+# if light == 'green':
+#     print('The light is green')
+#     print('That means you can')
+#     print('go')
+# elif light == 'red':
+#     print('The light is red')
+#     print('That means you should')
+#     print('stop')
+# elif light == 'amber':
+#     print("It's going to change soon")
+#     print('hurry up')
+# else:
+#     print("that's a weird color")
+#     print("rather stop")
+
+# emptyString = ''
+# if emptyString:
+#     print("yes")
+# else:
+#     print('its an empty string')
+#
+# zerovalue = 0
+# if zerovalue:
+#     print("its evaluated to true")
+# else:
+#     print('its evaluated to false')
+
+# inp = input("Type in something: ")
+# if inp:
+#     print(inp)
+# else:
+#     print('You have not typed anything')
+
+# a = 4
+# b = 5
+#
+# #not equal
+# if a != b:
+#     print("they are not equal")
+#
+# if light=='green' or light=='amber':
+#     print('go')
+#
+# weather = 'rainy'
+# boots = 'yes'
+#
+# if weather=='rainy' and boots=='yes':
+#     print('you may leave the house')
+#
+#
+# locked = False
+#
+# if not locked:
+#     print('lock the door!')
+#
+#
+
+# myList =['apples', 'oranges', '', 'bananas']
+# if not all(myList):
+#     print('you forgot something')
+#
+# for element in myList:
+#     print(element)
+
+myNumList = [4, 5, 8, 2, -9, 234532]
+for martina in myNumList:
+    print(martina + 7)
+
+
+
+# num1 = float(input("Type in your first number: "))
+# num2 = float(input("Type in your second number: "))
+# operator = input("Type in your operator: ")
+# if operator == '+':
+#     print(num1+num2)
+# elif operator == '-':
+#     print(num1-num2)
+
+# num = 0
+#
+# while num != 7:
+#     num = int(input("Type in a number: "))
+
